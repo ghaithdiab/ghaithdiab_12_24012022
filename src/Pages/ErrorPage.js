60e1715212ba165779cde3c3e0e6babdec47e59f
ghaithdiab <ghaith.diab@hotmail.com>
2022-02-06 09:59:05 +0100

    Error page

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -1,10 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`
+const Title=styled.h1`
+color: #e60000;
+font-weight: 700;
+font-size: 5rem;
+`
+    
+  const Text=styled.span`
+    color: #282d30;
+    font-weight: 500;
+    font-size: 3rem;
+  `
+    
 const ErrorPage=()=>{
   return (
-    <div>
-      ErrorPage
-    </div>
+    <Container>
+      <Title>404</Title>
+      <Text>Page non trouvée !</Text>
+    </Container>
   )
 }
 
